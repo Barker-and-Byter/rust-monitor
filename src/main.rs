35313841daa7a72224e19569ab02751b38109bb3
Barks-and-byter <jabarke2@our.ecu.edu.au>
2026-09-03 12:51:51 +0800

    added authentication for the rust back end to the svelte front end

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,33 @@
 use axum::{
     Router,
     response::sse::{Event, Sse},
-    routing::{any, get},
+    routing::{any, get, post},
+    http::StatusCode,
+    {Json, response::IntoResponse}
 };
+use time::SignedDuration;
+use std::{
+    collections::HashSet,
+    sync::{Mutex, OnceLock},
+};
+use rand::RngExt;
+use serde::Deserialize;
+use axum_extra::extract::{CookieJar, cookie::Cookie};
 use docker_wrapper::command::compose::start;
 pub use docker_wrapper::{DockerCommand, PsCommand, StatsCommand};
 use futures_util::{
     future::err,
     stream::{self, Stream},
 };
-use std::io::{self, Write};
+use std::{env, io::{self, Write}};
 use http::header::{AUTHORIZATION, CONTENT_TYPE};
 use std::{thread::sleep, time::Duration};
 pub use sysinfo::{Disks, Networks, System};
+use dotenv::dotenv;
 use tokio;
 use tokio::sync::mpsc;
 use tokio_stream::{StreamExt, wrappers::IntervalStream};
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::{AllowCredentials, AllowOrigin, Any, CorsLayer};
 
 //modules
 mod cpu_stats;
@@ -37,6 +48,19 @@ pub struct NetworkStats {
 pub struct DriveStats {
     pub written_bytes: f64,
     pub read_bytes: f64,
+}
+
+//struct for authentication
+#[derive(Deserialize)]
+struct AuthRequest {
+    secret: String,
+}
+
+//sessions storage for front end server 
+static SESSIONS: OnceLock<Mutex<HashSet<String>>> = OnceLock::new();
+
+fn sessions() -> &'static Mutex<HashSet<String>> {
+    SESSIONS.get_or_init(|| Mutex::new(HashSet::new()))
 }
 
 // struct for docker stats
@@ -152,12 +176,14 @@ async fn main() {
     // .unwrap_or_else(|_| ports[port_index].to_string());
 
     let cors = CorsLayer::new()
-        .allow_origin(Any)
+        .allow_origin(AllowOrigin::exact("http://localhost:5173".parse().unwrap()))
         .allow_methods([http::Method::GET, http::Method::POST, http::Method::OPTIONS])
-        .allow_headers([CONTENT_TYPE, AUTHORIZATION]);
+        .allow_headers([CONTENT_TYPE, AUTHORIZATION])
+        .allow_credentials(true);
 
     let app = Router::new()
         .route("/data-stream", get(sse_handler))
+        .route("/api/authenticate", post(authenticate_handler))
         .layer(cors);
 
     for port in ports {
@@ -188,7 +214,55 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, std::convert::Infallible>>> {
+
+async fn authenticate_handler (
+    jar: CookieJar,
+    Json(payload): Json<AuthRequest>,
+) -> impl IntoResponse {
+    dotenv().ok();
+    let expected_secret = env::var("PRIVATE_API_TOKEN").unwrap_or_default();
+
+    if payload.secret != expected_secret {
+        return (StatusCode::UNAUTHORIZED, jar).into_response();
+    }
+
+    let session_token: String = rand::rng()
+    .sample_iter(&rand::distr::Alphanumeric)
+    .take(32)
+    .map(char::from)
+    .collect();
+
+    sessions().lock().unwrap().insert(session_token.clone());
+
+    let cookie = Cookie::build(("auth_token", session_token))
+        .path("/")
+        .http_only(true)
+        .same_site(axum_extra::extract::cookie::SameSite::Lax)
+        .secure(false)
+        .max_age(SignedDuration::hours(12))
+        .build();
+   
+
+    (StatusCode::OK, jar.add(cookie)).into_response()
+}
+
+
+async fn sse_handler(
+    jar: CookieJar,
+) -> Result<Sse<impl Stream<Item = Result<Event, std::convert::Infallible>>>, StatusCode> {
+
+
+    let authorised =  jar
+    .get("auth_token")
+    .map(|cookie| cookie.value().to_string())
+    .map(|token| sessions().lock().unwrap().contains(&token))
+    .unwrap_or(false);
+
+    if !authorised {
+        return Err(StatusCode::UNAUTHORIZED);
+    }
+
+
     //create a channel to receive the information with a buffer of 5
     let (docker_tx, mut docker_rx) = mpsc::channel::<Vec<DockerStats>>(5);
     let (net_tx, mut net_rx) = mpsc::channel::<Vec<NetworkStats>>(5);
@@ -301,7 +375,7 @@ async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, std::convert::Inf
         Ok::<Event, std::convert::Infallible>(Event::default().data(json_payload))
     });
 
-    Sse::new(stream).keep_alive(
+    Ok(Sse::new(stream).keep_alive(
         axum::response::sse::KeepAlive::new().interval(std::time::Duration::from_secs(15)),
-    )
+    ))
 }
